7ede73580db50d7aef1ba010a7623f106866f9bc
woutverjans <wout.verjans@gmail.com>
2024-03-06 15:29:06 +0100

    in nieuwe klasse CheckNeighboursInGrid de methode getSameNeighboursIds

---
+++ mijnIntelliJProject/src/main/java/com/kuleuven/mijnintellijproject/CheckNeighboursInGrid.java
@@ -0,0 +1,16 @@
+package com.kuleuven.mijnintellijproject;
+
+public class CheckNeighboursInGrid {
+    /**
+     * This method takes a 1D Iterable and an element in the array and gives back an iterable containing the indexes of all neighbours with the same value as the specified element
+     *@return - Returns a 1D Iterable of ints, the Integers represent the indexes of all neighbours with the same value as the specified element on index 'indexToCheck'.
+     *@param grid - This is a 1D Iterable containing all elements of the grid. The elements are integers.
+     *@param width - Specifies the width of the grid.
+     *@param height - Specifies the height of the grid (extra for checking if 1D grid is complete given the specified width)
+     *@param indexToCheck - Specifies the index of the element which neighbours that need to be checked
+     */
+    public static Iterable<Integer> getSameNeighboursIds(Iterable<Integer> grid,int width, int height, int indexToCheck){
+        //write you code below
+        return result;
+    }
+}
